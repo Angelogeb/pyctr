56a0cfa3726fefc95d4f71a6fa72427d29d33b97
Angelogeb <any__955@hotmail.it>
2019-11-20 18:20:32 +0100

    Added multiple runs to the benchmark

--- pyctr/examples/torchscript/benchmark.py
+++ pyctr/examples/torchscript/benchmark.py
@@ -45,11 +45,12 @@ if __name__ == "__main__":
     seq_length = 1000
     state_dim = 64
     input_dim = 64
-    n_times = 10
+    epochs = 10
     state_cat_input_dim = state_dim + input_dim
     training = True
-    trace_training = torch.tensor([1])
+    trace_training = torch.tensor([1 if training else 0])
     dev = "cpu"
+    tests = 10
 
     seq = torch.randn(seq_length, batch_size, input_dim).to(dev)
     w = torch.randn([state_cat_input_dim, state_dim], requires_grad=True).to(dev)
@@ -59,28 +60,50 @@ if __name__ == "__main__":
     scripted_rnn = torch.jit.script(rnn_w_dropout)
     traced_rnn = torch.jit.trace(trace_rnn_w_dropout, (w, seq, trace_training))
 
-    vanilla_time = timeit(
-        lambda: full_bench(rnn_w_dropout, (w, seq, training), expected), number=n_times
-    )
-    specialize_time = timeit(
-        lambda: full_bench(specialized_rnn, (w, seq, training), expected),
-        number=n_times,
-    )
-    script_time = timeit(
-        lambda: full_bench(scripted_rnn, (w, seq, training), expected), number=n_times
-    )
-
-    trace_time = timeit(
-        lambda: full_bench(traced_rnn, (w, seq, trace_training), expected),
-        number=n_times,
-    )
+    times = [[], [], [], []]
 
     print(f"Running forward + backward benchmark (training={training})")
-    print(f"Time vanilla {vanilla_time}")
-    print(f"Time `pyctr.specialize` {specialize_time}")
-    print(f"Time `torch.jit.script` {script_time}")
-    print(f"Time `torch.jit.trace` {trace_time}")
+
+    for i in range(tests):
+
+        vanilla_time = timeit(
+            lambda: full_bench(rnn_w_dropout, (w, seq, training), expected),
+            number=epochs,
+        )
+        specialize_time = timeit(
+            lambda: full_bench(specialized_rnn, (w, seq, training), expected),
+            number=epochs,
+        )
+        script_time = timeit(
+            lambda: full_bench(scripted_rnn, (w, seq, training), expected),
+            number=epochs,
+        )
+
+        trace_time = timeit(
+            lambda: full_bench(traced_rnn, (w, seq, trace_training), expected),
+            number=epochs,
+        )
+
+        for j, t in enumerate([vanilla_time, specialize_time, script_time, trace_time]):
+            times[j].append(t)
+
+        print(f"Time vanilla {vanilla_time}")
+        print(f"Time `pyctr.specialize` {specialize_time}")
+        print(f"Time `torch.jit.script` {script_time}")
+        print(f"Time `torch.jit.trace` {trace_time}")
+
+        print(
+            f"Speedup wrt script: vanilla {script_time / vanilla_time}, specialize {script_time / specialize_time}, trace {script_time / trace_time}"
+        )
+
+    average_times = [sum(times[i]) / tests for i in range(len(times))]
+
+    print(f"Average time vanilla {average_times[0]}")
+    print(f"Average time `pyctr.specialize` {average_times[1]}")
+    print(f"Average time `torch.jit.script` {average_times[2]}")
+    print(f"Average time `torch.jit.trace` {average_times[3]}")
 
     print(
-        f"Time wrt script: vanilla {script_time / vanilla_time}, specialize {script_time / specialize_time}, trace {script_time / trace_time}"
+        f"Average speedup wrt script: vanilla {average_times[2] / average_times[0]},"
+        f"specialize {average_times[2] / average_times[1]}, trace {average_times[2] / average_times[3]}"
     )
