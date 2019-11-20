100b61d4f8cfa20cfe80e16fb53e63284e0a4bf6
Angelogeb <any__955@hotmail.it>
2019-11-20 17:52:16 +0100

    Added trace to benchmark

--- pyctr/examples/torchscript/benchmark.py
+++ pyctr/examples/torchscript/benchmark.py
@@ -6,16 +6,28 @@ from torch.jit.frontend import get_jit_def
 from pyctr.examples.torchscript import torchscript
 
 
-def foo(w, seq, training: bool):
-    ret = seq[0]  # initial state is first step
+def trace_rnn_w_dropout(w, seq, training):
+    out = seq[0]  # initial state is first step
+    bool_train = bool((training == 1).any())
     for i in range(len(seq) - 1):
-        h = torch.cat((seq[i], ret), dim=1)
-        ret = torch.tanh(h @ w)
-        if training:
-            ret = torch.dropout(ret, 0.5, training)
+        h = torch.cat((seq[i], out), dim=1)
+        out = torch.tanh(h @ w)
+        if bool_train:
+            out = torch.dropout(out, 0.2, True)
 
-    ret = torch.tanh(torch.cat((seq[-1], ret), dim=1) @ w)
-    return ret
+    out = torch.tanh(torch.cat((seq[-1], out), dim=1) @ w)
+    return out
+
+
+def rnn_w_dropout(w, seq, train: bool):
+    out = seq[0]
+    for i in range(len(seq) - 1):
+        h = torch.cat((seq[i], out), 1)
+        out = torch.tanh(h @ w)
+        if train:
+            out = torch.dropout(out, 0.2, True)
+    out = torch.tanh(torch.cat((seq[-1], out), 1) @ w)
+    return out
 
 
 def forward_bench(fun, args, expected):
@@ -36,35 +48,39 @@ if __name__ == "__main__":
     n_times = 10
     state_cat_input_dim = state_dim + input_dim
     training = True
+    trace_training = torch.tensor([1])
     dev = "cpu"
 
     seq = torch.randn(seq_length, batch_size, input_dim).to(dev)
     w = torch.randn([state_cat_input_dim, state_dim], requires_grad=True).to(dev)
-
-    specialized_foo, _ = torchscript.specialize(foo, w, seq, training)
-    jitted_foo = torch.jit.script(foo)
-
     expected = torch.ones(state_dim).to(dev)
 
+    specialized_rnn, _ = torchscript.specialize(rnn_w_dropout, w, seq, training)
+    scripted_rnn = torch.jit.script(rnn_w_dropout)
+    traced_rnn = torch.jit.trace(trace_rnn_w_dropout, (w, seq, trace_training))
+
+    vanilla_time = timeit(
+        lambda: full_bench(rnn_w_dropout, (w, seq, training), expected), number=n_times
+    )
+    specialize_time = timeit(
+        lambda: full_bench(specialized_rnn, (w, seq, training), expected),
+        number=n_times,
+    )
+    script_time = timeit(
+        lambda: full_bench(scripted_rnn, (w, seq, training), expected), number=n_times
+    )
+
+    trace_time = timeit(
+        lambda: full_bench(traced_rnn, (w, seq, trace_training), expected),
+        number=n_times,
+    )
+
     print(f"Running forward + backward benchmark (training={training})")
-    print(
-        f"Time vanilla {timeit(lambda: full_bench(foo, (w, seq, training), expected), number=n_times)}"
-    )
-    print(
-        f"Time `torch.jit.script` {timeit(lambda: full_bench(jitted_foo, (w, seq, training), expected), number=n_times)}"
-    )
-    print(
-        f"Time `pyctr.specialize` {timeit(lambda: full_bench(specialized_foo, (w, seq, training), expected), number=n_times)}"
-    )
-
+    print(f"Time vanilla {vanilla_time}")
+    print(f"Time `pyctr.specialize` {specialize_time}")
+    print(f"Time `torch.jit.script` {script_time}")
+    print(f"Time `torch.jit.trace` {trace_time}")
 
     print(
-        f"Time vanilla {timeit(lambda: full_bench(foo, (w, seq, training), expected), number=n_times)}"
+        f"Time wrt script: vanilla {script_time / vanilla_time}, specialize {script_time / specialize_time}, trace {script_time / trace_time}"
     )
-    print(
-        f"Time `torch.jit.script` {timeit(lambda: full_bench(jitted_foo, (w, seq, training), expected), number=n_times)}"
-    )
-    print(
-        f"Time `pyctr.specialize` {timeit(lambda: full_bench(specialized_foo, (w, seq, training), expected), number=n_times)}"
-    )
-
